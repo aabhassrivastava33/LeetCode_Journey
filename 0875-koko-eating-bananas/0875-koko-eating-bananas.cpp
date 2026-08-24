ace5800ceee3d6b183a18019c66be76bf9b62c3e
Aabhas Srivastava <aabhassrivastava33@gmail.com>
2026-08-24 06:32:58 +0530

    Time: 11 ms (35.17%), Space: 23 MB (22.82%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,56 +1,52 @@
 class Solution {
 public:
-    bool ispossible(vector<int>&piles , int mid , int h)
+    bool ispossible(vector<int>& piles, int mid ,int h)
     {
-        int hour = 0;
+        long long hour = 0;
         for(int i=0;i<piles.size();i++)
         {
-            if(piles[i]<=mid)
+            if(piles[i]<mid)
+            hour++;
+            else if (piles[i]%mid==0)
             {
-                hour++;
+                hour +=(piles[i]/mid);
             }
-            else if(piles[i]%mid==0)//current box contains multiple of mid 
-            {
-                hour +=(piles[i]/mid); 
-            }
-            else if(piles[i]%mid != 0)//current box does not contain multiple of mid 
+            else if(piles[i]%mid!=0)
             {
                 hour +=(piles[i]/mid)+1;
             }
             if(hour>h)
-            return false;
+            {
+                return false;
+            }
+        
         }
         return true;
     }
-
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi = INT_MIN;
-        for (int i=0;i<piles.size();i++)
-        {
-            if(piles[i]>maxi)
-            {
-                maxi=piles[i];
-            }
-        }
-    int n=piles.size();
-    int s = 1;
-    int e = maxi;
-    int ans;
-    while(s<=e)
+    int maxi=INT_MIN;
+    for(int i=0;i<piles.size();i++)
     {
-      //Assume atmost mid speed of koko
-      int mid = (s+e)/2;
-      if(ispossible(piles,mid,h))
-      {
-        ans = mid;
-        e = mid-1;  
-      }
-      else
-      {
-        s = mid+1;
-      }
-
+        if(piles[i]>maxi)
+        {
+            maxi=piles[i];
+        }
+    }
+    int start = 1;
+    int end = maxi;
+    int ans = 1;
+    while(start<=end)
+    {
+        int mid = start + (end - start) / 2;
+        if(ispossible(piles,mid,h))
+        {
+            ans=mid;
+            end = mid-1;
+        }
+        else
+        {
+            start = mid+1;
+        }
     }
     return ans;
     }
